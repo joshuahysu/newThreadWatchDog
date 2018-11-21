f5711c6768deb4be4b3c6b5251e1afc9d0bb93e8
蘇信宇 <joshuahysu@gmail.com>
2018-11-21 13:41:38 +0800

    test

--- newThreadWatchDog/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace newThreadWatchDog
-{
-    public class Class1
-    {
-    }
-}
---
+++ newThreadWatchDog/ThreadWatchDog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Data.SqlClient;
+
+namespace newThreadWatchDog
+{
+    public class ThreadWatchDog
+    {
+        public volatile bool boolIsRun=true;//break Thread while loop
+        public static string connectionString;//db connection String
+        private ThreadWatchDog() {
+        }
+        public ThreadWatchDog(Action newThreadFunc,string sql)//sql=$"insert WatchDogTable('ID','threadName','datetime') values('{Guid.new()}','{}','{datetime.now}')"
+        {
+            try
+            {
+                new Thread(() => {
+                    while (boolIsRun)
+                    {
+                        using (SqlConnection connection = new SqlConnection(connectionString))
+                        {
+                            connection.Open();
+                            using (SqlCommand cmd = new SqlCommand(sql, connection))
+                            {
+                                cmd.ExecuteNonQuery();
+                            }
+                        }
+                        newThreadFunc();
+                    }
+                }).Start();
+            }
+            catch { throw; }
+        }
+    }
+}
